23a930296715fd6e4a4ab40380228608b44a4127
malleus35 <malleus35@gmail.com>
2021-02-11 01:09:35 +0900

    add api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Headers, Delete } from '@nestjs/common';
 import { SignInDto } from './dto/SignInDto';
 import { SignUpDto } from './dto/SignUpDto';
 import { UsersService } from './users.service';
@@ -8,14 +8,32 @@ import { User } from './interfaces/user.interface';
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @Post('signup')
+  @Post()
   async signUp(@Body() signUpDto: SignUpDto) {
     this.usersService.create(signUpDto);
+  }
+
+  @Delete()
+  async deleteUser(@Headers('authorization') token: string) {
+    // this.usersService.delete(token);
   }
 
   @Post('signin')
   async signIn(@Body() signInDto: SignInDto) {
     this.usersService.signIn(signInDto);
+  }
+
+  @Post('logout')
+  async logOut(@Headers('authorization') token: string) {
+    // this.usersService.logout(token);
+  }
+
+  @Post('refresh')
+  async refresh(
+    @Headers('authorization') token: string,
+    @Headers('refresh') refresh: string,
+  ) {
+    // this.usersService.refresh(token, refresh);
   }
 
   @Get('all')
