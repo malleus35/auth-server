0e11fab160f79c3c96f10071f07010fce6488b4c
malleus35 <malleus35@gmail.com>
2021-02-12 12:15:41 +0900

    add passport, and start to authentication

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { SignInDto } from './dto/SignInDto';
 import { User } from './user.entity';
 
 @Injectable()
