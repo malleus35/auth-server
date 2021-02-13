9e473629f1d18abf93ab37d2e1e58040def27614
malleus35 <malleus35@gmail.com>
2021-02-13 16:16:35 +0900

    try to add entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,44 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-// import { User } from './user.entity';
-
-export type User = any;
+import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
-  // constructor() {} // @InjectRepository(User) private usersRepository: Repository<User>,
-
-  // async signIn(user: User): Promise<void> {
-  //   await this.usersRepository.save(user);
-  // }
-
-  // async create(user: User): Promise<void> {
-  //   await this.usersRepository.save(user);
-  // }
-
-  // async findOne(idx: number): Promise<User> {
-  //   const test = await this.usersRepository.findOne(idx);
-  //   console.log(test);
-  //   return await this.usersRepository.findOne(idx);
-  // }
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username);
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+  async signIn(user: User): Promise<void> {
+    await this.usersRepository.save(user);
   }
-
-  // findAll(): Promise<User[]> {
-  //   return this.usersRepository.find();
-  // }
+  async create(user: User): Promise<void> {
+    await this.usersRepository.save(user);
+  }
+  async findOne(idx: number): Promise<User> {
+    const test = await this.usersRepository.findOne(idx);
+    console.log(test);
+    return await this.usersRepository.findOne(idx);
+  }
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
 }
