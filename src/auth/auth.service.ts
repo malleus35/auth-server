ba2e877cea01907fd70de0d89096c8c5defd2ae6
malleus35 <malleus35@gmail.com>
2021-02-13 14:01:04 +0900

    add jwtContants and login function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { SignInDto } from 'src/users/dto/SignInDto';
 import { User } from 'src/users/user.entity';
 import { UsersService } from '../users/users.service';
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string): Promise<User> {
     const idx = 1;
@@ -17,5 +21,12 @@ export class AuthService {
     }
     return null;
     // return user;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
